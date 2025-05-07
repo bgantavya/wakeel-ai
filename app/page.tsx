fa13d9f397fe4a7d9bfd8ec7fba64de05a4c86f9
stubanx <gantavyaoo@gmail.com>
2025-05-07 23:45:19 +0530

    new file:   app/ModeToggle.tsx 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components/theme-provider.tsx 	new file:   components/ui/button.tsx 	new file:   components/ui/dropdown-menu.tsx 	new file:   components/ui/form.tsx 	new file:   components/ui/label.tsx 	new file:   components/ui/next 	modified:   package-lock.json 	modified:   package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
-
+// import Image from "next/image";
+import {Button} from "@/components/ui/button"
+import {ModeToggle} from "./ModeToggle"
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>
+        Wakeel AI
+      </h1>
+      <ModeToggle/>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        ShadCN@2.3.0(Next.js)
+        <Button>
+          ShadCN@2.3.0(Next.js)
+        </Button>
       </main>
     </div>
   );
+  
 }
